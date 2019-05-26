f93aa92921fa97b3a2f8935bc44cabc1884389e9
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-05-26 14:11:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+var GalleryItem = React.createClass({
+    propTypes: {
+      image: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {},
+          React.createElement('h2', {}, this.props.image.name),
+          React.createElement('img', {src: this.props.image.src})
+        )
+      )
+    },
+  });
+
+  var image = {
+    name: 'Kotek',
+    src: 'http://imgur.com/n8OYCzR.png'
+  };
+
+// Movie
+  var Movie = React.createClass({
+    propTypes: {
+      movie: React.PropTypes.object.isRequired,
+    },
+
+    render: function() {
+      return (
+        React.createElement('li', {key: movie.id},
+          React.createElement('div', {},
+            React.createElement('h2', {}, this.props.movie.name),
+            React.createElement('img', {src: this.props.movie.src})
+          )
+        )
+      )
+    },
+  });
+  
+  var movie = {
+    id: 1,
+    name: 'Kotek-movie',
+    src: 'https://www.pexels.com/video/video-of-funny-cat-855029/'
+  };
+
+// MovieTitle
+  var MovieTitle = React.createClass({
+    propTypes: {
+      movietitle: React.PropTypes.object.isRequired,
+    },
+
+    render: function() {
+      return (
+          React.createElement('div', {},
+            React.createElement('h2', {}, this.props.movietitle.title),
+          )
+        )
+    },
+  });
+  
+  var movietitle = {
+    title: 'test'
+  };
+
+// MovieDescription
+var MovieDescription = React.createClass({
+  propTypes: {
+    moviedescription: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+        React.createElement('div', {},
+          React.createElement('p', {}, this.props.moviedescription.description),
+        )
+      )
+  },
+});
+
+var moviedescription = {
+  description: 'bla bla bla'
+};
+
+// MoviesList
+var MoviesList = React.createClass({
+  propTypes: {
+    movieslist: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+        React.createElement('div', {},
+          React.createElement('p', {}, this.props.movieslist.list),
+        )
+      )
+  },
+});
+
+var movieslist = {
+  list: 'test list',
+};
+// end
+
+  var element = 
+    React.createElement('div', {},
+      React.createElement(GalleryItem, {image: image}),
+      React.createElement(Movie, {key: movie.id, movie: movie},
+        React.createElement(MovieTitle, {movietitle: movietitle}),
+        React.createElement(MovieDescription, {moviedescription: moviedescription}),
+        React.createElement(MoviesList, {movieslist: movieslist})
+      )
+    );
+
+  ReactDOM.render(element, document.getElementById('app'));
